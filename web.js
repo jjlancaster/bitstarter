89efcff0834e8ac7190def7b934484e9b7b0c550
Justin Lancaster <justin.lancaster@att.net>
2013-07-27 05:34:43 +0000

    Updated web.js to read from index.html

--- web.js
+++ web.js
@@ -1,12 +1,7 @@
-var express = require('express');
-var app = express();
-app.use(express.logger());
+var buf = new Buffer(" ");
+fs=require("fs");
+buf = fs.readFileSync('index.html');
+var buf_write = buf.toString('utf8', 0, 27 );
+console.log(buf_write);
 
-app.get('/', function(request, response) {
-  response.send('Hello World2!');
-});
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
