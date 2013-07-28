bddd9f8338782262d0a9981ef74c0da38e2d1378
Justin Lancaster <justin.lancaster@att.net>
2013-07-28 19:45:22 +0000

    revised web.js to feed buf.toString into buf2

--- web.js
+++ web.js
@@ -1,12 +1,13 @@
 var buf = new Buffer(256);
+var buf2 = null ;
 fs=require("fs");
 buf = fs.readFileSync('index.html');
-buf.toString();
+buf2 = buf.toString();
 var express = require('express');
 var app = express();
 app.use(express.logger());
 app.get('/', function(request, response) {
-  response.send(buf);
+  response.send(buf2);
   });
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
