51d5fdcafdcb4f9f25b1d5ec450315e60f1f0d36
Justin Lancaster <justin.lancaster@att.net>
2013-07-28 19:03:03 +0000

    revised buffer dimension to 256 and simplified buf

--- web.js
+++ web.js
@@ -1,29 +1,15 @@
-
-// These next four lines work, but won't deploy properly on Heroku alone
-// therefore, merge somehow?? try putting buf_write into the send function?
-//
-// So, first read index.html and get into buffer, convert to string variable
-var buf = new Buffer(" ");
+var buf = new Buffer(256);
 fs=require("fs");
 buf = fs.readFileSync('index.html');
-var buf_write = buf.toString('utf8', 0, 27 );
-//console.log(buf_write);
-
-// Below is the original web.js that deployed correctly on Heroku
-//
-// Then, use the prior web.js functions and call the buff_write string with send
+buf.toString();
 var express = require('express');
 var app = express();
 app.use(express.logger());
-
-
 app.get('/', function(request, response) {
-  response.send(buf_write);
-});
-
-
+  response.send(buf);
+  });
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
-});
+  });
 
